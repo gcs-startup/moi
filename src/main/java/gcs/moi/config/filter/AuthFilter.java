17b3f7859e7c58f9666069cd9d4efb6d01ffb94f
sungyujeon <sungyujeon91@gmail.com>
2023-12-03 00:32:39 +0900

    fix: unauthorized 로직 변경

--- src/main/java/gcs/moi/config/filter/AuthFilter.java
+++ src/main/java/gcs/moi/config/filter/AuthFilter.java
@@ -2,15 +2,17 @@ package gcs.moi.config.filter;
 
 import gcs.moi.auth.web.AuthenticatedMember;
 import gcs.moi.auth.web.SessionProvider;
+import gcs.moi.config.exception.MoiApplicationException;
 import gcs.moi.domain.Member;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
@@ -23,15 +25,21 @@ public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
         String requestURI = httpRequest.getRequestURI();
 
-        boolean isMatchedURI = Arrays.asList("", "/api/v1/member/join", "/api/v1/member/login")
-                .stream()
-                .anyMatch(requestURI::startsWith);
-
+        String[] permitAll = {"/swagger-ui", "/v3/api-docs", "/api/v1/member/join", "/api/v1/member/login" };
+        boolean isMatchedURI = Stream.of(permitAll)
+                                     .anyMatch(requestURI::startsWith);
         if (!isMatchedURI) {
-            Member member = sessionProvider.getMemberBySessionId(httpRequest.getSession().getId());
-            authenticatedMember.setMember(member);
+            Member member;
+            try {
+                member = sessionProvider.getMemberBySessionId(httpRequest.getSession().getId());
+                authenticatedMember.setMember(member);
+            } catch (MoiApplicationException e) {
+                httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
         }
 
         chain.doFilter(request, response);
